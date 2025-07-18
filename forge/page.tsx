367a235502cf3a12544172a96bb1a8727217d6c4
robinjusticeleague <robinjusticeleague@gmail.com>
2025-07-18 14:29:14 +0000

    feat: now have to remove files when there icons has been removed from tsx file

--- forge/page.tsx
+++ forge/page.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 
-// This can be any component.
 const MyComponent = () => {
   return (
     <div>
       <h1>Hello from DX!</h1>
-      {/* This should now work perfectly! */}
       <Dx.ArrowRight />
-      
     </div>
   );
 };
