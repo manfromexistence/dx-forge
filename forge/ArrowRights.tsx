3aecaa4de706210675b041a4e0e99f03c4644835
robinjusticeleague <robinjusticeleague@gmail.com>
2025-07-18 14:28:25 +0000

    feat: icon generation is working fine

---
+++ forge/ArrowRights.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ArrowRights = () => {{
+  return (
+    <div>
+      {/* TODO: Implement the ArrowRights icon */}
+      <span>{ArrowRights}</span>
+    </div>
+  );
+}};
+
+export default ArrowRights;
