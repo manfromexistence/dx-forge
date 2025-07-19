6d4df062228efa9d0a11fe4ba27237683bf2ff58
robinjusticeleague <robinjusticeleague@gmail.com>
2025-07-19 22:31:11 +0000

    feat: cli - ui

--- src/cli/prompts/mod.rs
+++ src/cli/prompts/mod.rs
@@ -1,13 +1,13 @@
 mod action;
 mod confirm;
 mod custom_type;
-#[cfg(feature = "date")]
-mod dateselect;
-#[cfg(feature = "editor")]
-mod editor;
+// #[cfg(feature = "date")]
+// mod dateselect;
+// #[cfg(feature = "editor")]
+// mod editor;
 mod multiselect;
 mod one_liners;
-mod password;
+// mod password;
 mod prompt;
 mod select;
 #[cfg(test)]
@@ -17,13 +17,13 @@ mod text;
 pub use action::*;
 pub use confirm::*;
 pub use custom_type::*;
-#[cfg(feature = "date")]
-pub use dateselect::*;
-#[cfg(feature = "editor")]
-pub use editor::*;
+// #[cfg(feature = "date")]
+// pub use dateselect::*;
+// #[cfg(feature = "editor")]
+// pub use editor::*;
 pub use multiselect::*;
 #[cfg(feature = "one-liners")]
 pub use one_liners::*;
-pub use password::*;
+// pub use password::*;
 pub use select::*;
 pub use text::*;
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,34 @@
-mod chronicle;
-mod generator;
-mod observer;
+use inquire::Text;
 
-#[tokio::main]
-async fn main() -> anyhow::Result<()> {
-    println!("DX: Initializing...");
+fn main() {
+    let name = Text::new("What is your name?").prompt();
 
-    let chronicle_repo = match chronicle::initialize() {
-        Ok(repo) => repo,
-        Err(e) => {
-            eprintln!("DX Error: Failed to initialize the Chronicle: {}", e);
-            return Err(e);
-        }
-    };
-
-    if let Err(e) = observer::start(chronicle_repo.clone()).await {
-        eprintln!("DX Error: The observer failed with an error: {}", e);
+    match name {
+        Ok(name) => println!("Hello {name}"),
+        Err(_) => println!("An error happened when asking for your name, try again later."),
     }
-
-    println!("DX: Shutting down.");
-    Ok(())
 }
+
+// mod chronicle;
+// mod generator;
+// mod observer;
+
+// #[tokio::main]
+// async fn main() -> anyhow::Result<()> {
+//     println!("DX: Initializing...");
+
+//     let chronicle_repo = match chronicle::initialize() {
+//         Ok(repo) => repo,
+//         Err(e) => {
+//             eprintln!("DX Error: Failed to initialize the Chronicle: {}", e);
+//             return Err(e);
+//         }
+//     };
+
+//     if let Err(e) = observer::start(chronicle_repo.clone()).await {
+//         eprintln!("DX Error: The observer failed with an error: {}", e);
+//     }
+
+//     println!("DX: Shutting down.");
+//     Ok(())
+// }
