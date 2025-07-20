6c12d4192d05f823a87df4ec18bf39813a19e5c7
robinjusticeleague <robinjusticeleague@gmail.com>
2025-07-20 10:00:23 +0000

    feat: inquirer

--- src/prompts/mod.rs
+++ src/prompts/mod.rs
@@ -1,10 +1,10 @@
 mod action;
 mod confirm;
 mod custom_type;
-#[cfg(feature = "date")]
-mod dateselect;
-#[cfg(feature = "editor")]
-mod editor;
+// #[cfg(feature = "date")]
+// mod dateselect;
+// #[cfg(feature = "editor")]
+// mod editor;
 mod multiselect;
 mod one_liners;
 mod password;
@@ -17,10 +17,10 @@ mod text;
 pub use action::*;
 pub use confirm::*;
 pub use custom_type::*;
-#[cfg(feature = "date")]
-pub use dateselect::*;
-#[cfg(feature = "editor")]
-pub use editor::*;
+// #[cfg(feature = "date")]
+// pub use dateselect::*;
+// #[cfg(feature = "editor")]
+// pub use editor::*;
 pub use multiselect::*;
 #[cfg(feature = "one-liners")]
 pub use one_liners::*;
