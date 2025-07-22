9ce0abc64bf4165b6342e720e6de3948d5cd4be6
robinjusticeleague <robinjusticeleague@gmail.com>
2025-07-22 08:01:19 +0000

    feat: will structure src folder

--- build.rs
+++ build.rs
@@ -1,12 +1,7 @@
-// build.rs
-
 fn main() {
-    // This tells the `cc` crate to find our C source file.
     cc::Build::new()
-        .file("c_src/file_creator.c")
-        .compile("file_creator_lib"); // The output will be `libfile_creator_lib.a`
-
-    // This tells Cargo to re-run the build script if our C code changes.
-    println!("cargo:rerun-if-changed=c_src/file_creator.c");
-    println!("cargo:rerun-if-changed=c_src/file_creator.h");
+        .file("src/generator/file_creator.c")
+        .compile("file_creator_lib");
+    println!("cargo:rerun-if-changed=src/generator/file_creator.c");
+    println!("cargo:rerun-if-changed=src/generator/file_creator.h");
 }
