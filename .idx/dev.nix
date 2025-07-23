de667852d67ec496cfcab727c3415a3e9581d57a
robinjusticeleague <robinjusticeleague@gmail.com>
2025-07-23 11:13:54 +0000

    feat: docker in dx by firebase studio

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -6,9 +6,7 @@
     pkgs.busybox
     pkgs.gcc
     pkgs.neovim
-    pkgs.mingw-w64
   ];
-
   env = { };
   idx = {
     extensions = [
@@ -19,25 +17,9 @@
     ];
     workspace = {
       onCreate = {
-        install = ''
-                    echo "Setting up native cross-compilation for Windows..."
-
-                    # 1. Add the rust target for Windows
-                    rustup target add x86_64-pc-windows-gnu
-
-                    # 2. Create the Cargo config file to specify the Windows C linker
-                    mkdir -p .cargo
-                    cat <<EOT > .cargo/config.toml
-          [target.x86_64-pc-windows-gnu]
-          linker = "x86_64-w64-mingw32-gcc"
-          ar = "x86_64-w64-mingw32-ar"
-          EOT
-
-                    echo "Setup complete. You can now build using 'cargo build --target...'"
-        '';
+        install = "rustup default stable && rustup update && cargo run";
         default.openFiles = [
           "README.md"
-          ".cargo/config.toml"
         ];
       };
     };
