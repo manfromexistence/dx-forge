351f2085a15783bb7b1a9ffb4aa27ca413cb4dd7
robinjusticeleague <robinjusticeleague@gmail.com>
2025-07-23 10:41:22 +0000

    feat: docker in dx by firebase studio

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -7,8 +7,12 @@
     pkgs.gcc
     pkgs.neovim
     pkgs.cargo-cross
-    pkgs.docker
+    # No need for pkgs.docker here, it's included by the option below
   ];
+
+  # Add this line to start the Docker daemon automatically
+  virtualisation.docker.enable = true;
+
   env = { };
   idx = {
     extensions = [
@@ -19,6 +23,8 @@
     ];
     workspace = {
       onCreate = {
+        # It's good practice to wait for the Docker daemon to be ready
+        # However, for a simple build command it should be fine.
         install = "rustup default stable && rustup update && cargo run";
         default.openFiles = [
           "README.md"
