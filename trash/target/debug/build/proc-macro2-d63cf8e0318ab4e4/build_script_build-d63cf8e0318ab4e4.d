8ac7795181323efa85517503685ea9fa36a3f872
robinjusticeleague <robinjusticeleague@gmail.com>
2025-07-26 11:05:18 +0000

    feat: dx cli

--- trash/target/debug/build/proc-macro2-d63cf8e0318ab4e4/build_script_build-d63cf8e0318ab4e4.d
+++
@@ -1,5 +0,0 @@
-/home/user/dx-forge/trash/target/debug/build/proc-macro2-d63cf8e0318ab4e4/build_script_build-d63cf8e0318ab4e4.d: /home/user/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.95/build.rs
-
-/home/user/dx-forge/trash/target/debug/build/proc-macro2-d63cf8e0318ab4e4/build_script_build-d63cf8e0318ab4e4: /home/user/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.95/build.rs
-
-/home/user/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/proc-macro2-1.0.95/build.rs:
